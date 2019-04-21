7843f2f27bdcc38922f605a0953fb5012a81b9a7
Huong-DTT <thuhuong160994@gmail.com>
2019-04-22 06:28:18 +0700

    add bt di chuyen doi tuong

---
+++ javascript.js
@@ -0,0 +1,57 @@
+function Hero(image, top, left, size){
+    this.image = image;
+    this.top = top;
+    this.left = left;
+    this.size = size;
+
+    this.getHeroElement = function(){
+        return '<img width="'+ this.size + '"' +
+            ' height="'+ this.size + '"' +
+            ' src="' + this.image +'"' +
+            ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+    };
+
+    this.moveRight = function(){
+        this.left += 10;
+        console.log('ok: ' + this.left);
+    };
+    this.moveDown = function() {
+        this.top += 10;
+    };
+
+    this.moveLeft = function () {
+        this.left-= 10;
+    };
+
+    this.moveTop = function () {
+        this.top -= 10;
+    };
+}
+
+let hero = new Hero('car.png', 20, 30, 100);
+function start(){
+        if(hero.left < window.innerWidth - hero.size && hero.top === 20) {
+            hero.moveRight();
+        } //hero.left = 1170; hero.size = 200; hero.top = 20;
+
+
+        if (hero.left >=  window.innerWidth - hero.size && hero.top < window.innerHeight - hero.size) {
+            hero.moveDown()
+        } //hero.top = 480;
+
+        if (hero.top >=(window.innerHeight - hero.size) &&  hero.left > 30) {
+            hero.moveLeft()
+        }
+        //hero.left = 30
+
+        if (hero.left <= 30 && hero.top > 20) {
+            hero.moveTop()
+        }
+        //hero.left = 30; hero.top = 20 trở về vị trí đầu tiên để quay lại vòng if thứ nhất
+
+
+        document.getElementById('game').innerHTML = hero.getHeroElement();
+        setTimeout(start, 100);
+}
+
+start();
